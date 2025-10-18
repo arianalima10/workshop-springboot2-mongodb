00223e2efde899ec04fb7df6b226a3cebee3fcdc
arianalima10 <ariana.crodrigues@gmail.com>
2025-10-17 22:44:54 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/arianalima/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.arianalima.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arianalima.workshopmongo.domain.User;
+import com.arianalima.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
